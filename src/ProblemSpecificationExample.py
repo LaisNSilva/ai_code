038eef767d3001394a02430dd62120ab5acbe94d
LaisNSilva <49496164+LaisNSilva@users.noreply.github.com>
2022-09-13 12:01:08 -0300

    mapa ok

--- src/ProblemSpecificationExample.py
+++ src/ProblemSpecificationExample.py
@@ -1,19 +1,28 @@
-from aicode.search.SearchAlgorithms import BuscaProfundidadeIterativa
+from aicode.search.SearchAlgorithms import BuscaProfundidadeIterativa, BuscaCustoUniforme
 from aicode.search.Graph import State
 
 class ProblemSpecification(State):
 
-    def __init__(self, op):
+    def __init__(self, cid_fim, cid_atual, custo, op):
+        #self.cid_inicio = cid_inicio
+        self.cid_fim = cid_fim
+        self.cid_atual = cid_atual
+        self.custo = custo
         self.operator = op
-        #TODO
+        
     
     def sucessors(self):
         sucessors = []
-        #TODO
+        mapa = self.createArea()
+        for t in mapa[self.cid_atual]:
+            sucessors.append(ProblemSpecification(self.cid_fim, t[1], self.custo+t[0], f'Da cidade {self.cid_atual} para {t[1]}, custo atual {self.custo+t[0]}'))
+
         return sucessors
     
     def is_goal(self):
-        pass
+        if self.cid_atual == self.cid_fim:
+            return True
+        return False
     
     def description(self):
         return "Describe the problem"
@@ -44,11 +53,56 @@ class ProblemSpecification(State):
         #
         None
 
+    #@staticmethod
+    def createArea(self):
+        #
+        # TODO mover a definicao do mapa de uma forma hard-coded para para leitura
+        # a partir de um arquivo, similar ao que é feito no metodo createHeuristics()
+        # 
+        Mapa = {
+            'a':[(3,'b'),(6,'c')],
+            'b':[(3,'a'),(3,'h'),(3,'k')],
+            'c':[(6,'a'),(2,'g'),(3,'d'),(2,'o'),(2,'p')],
+            'd':[(3,'c'),(1,'f'),(1,'e')],
+            'e':[(2,'i'),(1,'f'),(1,'d'),(14,'m')],
+            'f':[(1,'g'),(1,'e'),(1,'d')],
+            'g':[(2,'c'),(1,'f'),(2,'h')],
+            'h':[(2,'i'),(2,'g'),(3,'b'),(4,'k')],
+            'i':[(2,'e'),(2,'h')],
+            'l':[(1,'k')],
+            'k':[(1,'l'),(3,'n'),(4,'h'),(3,'b')],
+            'm':[(2,'n'),(1,'x'),(14,'e')],
+            'n':[(2,'m'),(3,'k')],
+            'o':[(2,'c')],
+            'p':[(2,'c')],
+            'x':[(1,'m')]
+            }
+        return Mapa
+
+
 
 def main():
     print('Busca em profundidade iterativa')
-    state = ProblemSpecification('')
+    #cid_inicio = 'o'
+    cid_fim = 'i'
+    cid_atual = 'o'
+    custo = 0
+    state = ProblemSpecification(cid_fim, cid_atual, custo, '')
     algorithm = BuscaProfundidadeIterativa()
+    result = algorithm.search(state)
+    if result != None:
+        print('Achou!')
+        print(result.show_path())
+    else:
+        print('Nao achou solucao')
+
+    print('Busca Custo Uniforme')
+    #cid_inicio = 'o'
+    cid_fim = 'i'
+    cid_atual = 'o'
+    custo = 0
+    state = ProblemSpecification(cid_fim, cid_atual, custo, '')
+    algorithm = BuscaCustoUniforme()
     result = algorithm.search(state)
     if result != None:
         print('Achou!')
